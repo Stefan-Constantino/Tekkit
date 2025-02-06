a688d812f2d49c0ef67c4803a957856dcef3b93c
unknown <stefan.constantino@stonybrook.edu>
2025-02-05 21:00:28 -0500

    small changed to hero section + css

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -2,13 +2,21 @@ import React from "react";
 
 function HeroSection() {
     return (
-        <section id="home">
-            <div className="container">
-                <h2>Welcome to Our IT Services</h2>
-                <p>Providing top-notch IT solutions for your business needs.</p>
-                <a href="#contact" className="btn">Get Started</a>
+        <div id="home">
+            <div id="HeroContainer">
+                 <div>
+                    <h2>Manage. Secure. Defend. </h2>
+                    <h1>Let us handle all your IT needs</h1>
+                    <div></div>
+                </div>
+                <div>
+                    <h2>Welcome to Our IT Services</h2>
+                    <p>Providing top-notch IT solutions for your business needs.</p>
+                    <a href="#contact" className="btn">Get Started</a>
+                </div>
+                
             </div>
-        </section>
+        </div>
     );
 }
 
