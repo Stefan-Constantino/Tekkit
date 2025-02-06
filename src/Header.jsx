60a699aa8b78f12d406c0c7d09cafbb513535815
Tony <anthonyh2020@gmail.com>
2025-02-06 02:21:52 -0500

    Top Header section with contact info

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className="supHeader">
+            <div>
+                <ul>
+                    <nav>
+                        <li><a href="#supCenter">Client Support Center</a></li>
+                        <li><a>Client Support: 646-XXX-XXXX</a></li>
+                        <li><a>Sales Inquiries: 646-XXX-XXXX</a></li>
+                    </nav>
+                </ul>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
