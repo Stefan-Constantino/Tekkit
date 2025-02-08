18705a7d3f72041c7b583d5d607e24b60287afff
Tony <anthonyh2020@gmail.com>
2025-02-07 22:46:50 -0500

    changed href to links to clean up home page

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 function Navbar() {
     return (
         <header>
             <div id="navbar-container">
-                <img id="logo" src="tekkit.png" alt="Tekk-It Logo" />
+                <Link to="/"><img id="logo" src="tekkit.png" alt="Tekk-It Logo" /></Link>
                 <nav>
                     <ul>
-                        <li><a href="#home">Home</a></li>
-                        <li><a href="#services">Services</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#contact">Contact</a></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/#services">Services</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/contact">Schedule Appointment</Link></li>
                     </ul>
                 </nav>
             </div>
