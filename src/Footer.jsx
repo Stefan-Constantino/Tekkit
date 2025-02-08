961f21d781b4ccd4a29ca31ef02bf96c2b18cb5e
Tony <anthonyh2020@gmail.com>
2025-02-07 20:50:12 -0500

    addition of social media icons on footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import SocialMediaIcons from "./Links";
 
 function Footer() {
     return (
         <footer>
             <div className="container">
                 <p>&copy; 2025 Tekk-It. All rights reserved.</p>
+                <SocialMediaIcons />
             </div>
         </footer>
     );
