e14add4cf9e27e9e223506e70ce69753612f6458
Tony <anthonyh2020@gmail.com>
2025-02-07 22:45:36 -0500

    Additions of "team" and "mission"

--- src/About.jsx
+++ src/About.jsx
@@ -7,6 +7,16 @@ function About() {
                 <h2>About Us</h2>
                 <p>We are dedicated to providing exceptional IT support with a focus on customer satisfaction.</p>
             </div>
+
+            <div id="team">
+                <h3>Our Team</h3>
+                <p>Meet our talented and passionate team members!</p>
+            </div>
+
+            <div id="mission">
+                <h3>Our Mission</h3>
+                <p>Our mission is to innovate and provide solutions that empower businesses to thrive in the digital age.</p>
+            </div>
         </section>
     );
 }
