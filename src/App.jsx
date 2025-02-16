836dcd7582bfa69dd0a0e7d918a4f9af70626958
Tony <anthonyh2020@gmail.com>
2025-02-16 03:04:33 -0500

    Addition of imports and removed ticket routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Header from "./Header";
 import Navbar from "./Navbar";
 import HeroSection from "./HeroSection";
-import Services from "./Services";
+import Services from "./services";
 import About from "./About";
 import Contact from "./Contact";
 import Footer from "./Footer";
@@ -11,14 +11,20 @@ import Problem from "./Problem";
 import Solution from "./Solution";
 import "./style.css";
 import AboutUsList from "./AboutUsList";
-import Ticket from "./Ticket";
+// import Ticket from "./Ticket";
 import MapComponent from "./MapComponent";
+import ServicesSection from "./servicesSection";
+import ServicesSection1 from "./servicesSection1";
+import ServicesSection2 from "./servicesSection2";
+import ServicesSection3 from "./ServicesSection3";
+import ServicesSection4 from "./servicesSection4";
+import ServicesSection5 from "./servicesSection5";
 
 function App() {
     return (
         <div className="app-container flex flex-col min-h-screen">
             <div className="maincontent flex-grow">
-                <Header />
+                {/* <Header /> */}
                 
                 {/* Define Routes here */}
                 <Routes>
@@ -33,9 +39,22 @@ function App() {
                             <Solution />
                         </>
                     } />
+                    <Route path="/services" element={
+                        <>
+                            <div id="home1">
+                                <Navbar />
+                            </div>
+                            <ServicesSection />
+                            <ServicesSection1 />
+                            <ServicesSection2 />
+                            <ServicesSection3 />
+                            <ServicesSection4 />
+                            <ServicesSection5 />
+                        </> 
+                    } />
                     <Route path="/contact" element={
                         <>
-                            <div id="home">
+                            <div id="home1">
                                 <Navbar />
                             </div>
                             <div className="row">
@@ -46,21 +65,21 @@ function App() {
                     } />
                     <Route path="/about" element={
                         <>
-                            <div id="home">
+                            <div id="home1">
                                 <Navbar />
                             </div>
                             <About />
                             <AboutUsList />
                         </> 
                     } />
-                    <Route path="/ticket" element={
+                    {/* <Route path="/ticket" element={
                         <>
                             <div id="home">
                                 <Navbar />
                             </div>
                             <Ticket />
                         </> 
-                    } />
+                    } /> */}
                 </Routes>
                 
             </div>
